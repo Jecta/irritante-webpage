2831947c8caf95fda5f5925cf3e926fd6a75ee57
Justin Bos <justin_bos@home.nl>
2023-12-07 10:04:47 +0100

    login form

--- javascript.js
+++ javascript.js
@@ -2,6 +2,10 @@ function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
 
+function openform() {
+  document.getElementById("myform").style.display = "block";
+}
+
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
