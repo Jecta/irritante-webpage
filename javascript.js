01b57466e65cc96ff6a75db8385dc4f769dfefda
Jeremy Bosma <jectaunscripted@gmail.com>
2023-12-13 11:49:02 +0100

    Add rickroll animation and audio

--- javascript.js
+++ javascript.js
@@ -12,9 +12,13 @@ function closeForm() {
 
 let rickroll = "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png";
 
-document.getElementById("phone").addEventListener("click", function() {
+document.getElementById("phone").addEventListener("click", function () {
   document.getElementById("phone").src = rickroll;
   document.getElementById("phone").style.animationName = "phone";
   document.getElementById("phone").style.animation = "phone 0.5s linear infinite";
   document.getElementById("phone").style.animationDelay = "1.5s";
+
+  var audio = new Audio("assets/nevergonnagiveyouup.mp3");
+  audio.loop = true;
+  audio.play();
 })
