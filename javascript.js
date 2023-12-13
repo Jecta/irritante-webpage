1b7dbddabf88dfb20cbe4509a03d807cd5a53787
Jeremy Bosma <jectaunscripted@gmail.com>
2023-12-13 11:44:57 +0100

    Add animation and click event to phone image

--- javascript.js
+++ javascript.js
@@ -9,3 +9,12 @@ function openform() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+let rickroll = "https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png";
+
+document.getElementById("phone").addEventListener("click", function() {
+  document.getElementById("phone").src = rickroll;
+  document.getElementById("phone").style.animationName = "phone";
+  document.getElementById("phone").style.animation = "phone 0.5s linear infinite";
+  document.getElementById("phone").style.animationDelay = "1.5s";
+})
